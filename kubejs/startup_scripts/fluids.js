f7346487af73cd2fdf8d64e32a3f32371397c78c
skall0900 <artemka03ver@gmail.com>
2026-09-24 15:27:27 +0700

    fix

--- kubejs/startup_scripts/fluids.js
+++
@@ -1,6 +0,0 @@
-StartupEvents.registry('fluid', event => {
-	event.create('catalyst_fluid')
-		.stillTexture('kubejs:fluid/catalyst_fluid_still')
-		.flowingTexture('kubejs:fluid/catalyst_fluid_still')
-		.noBlock
-})
